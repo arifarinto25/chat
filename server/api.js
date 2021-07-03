445b1e10b140a3f3591efb06d125e4b41e7f7e3d
arifarinto25 <arif4rinto@gmail.com>
2021-07-03 19:30:13 +0700

    first commit

---
+++ server/api.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const io = require("socket.io-client");
+const socket = io("ws://localhost:3000");
+
+app.get('/helloworld/:from/:to/:message', (req, res) => {
+  res.send('Hello World!')
+  const content = req.params.message
+  const to = req.params.to
+  const message = {
+    content,
+    from: req.params.from,
+    to,
+  };
+  socket.to(to).to(req.params.from).emit("private message", message);
+  console.log(message);
+  messageStore.saveMessage(message);
+});
+
+app.listen(port, () => {
+  console.log(`cli-nodejs-api listening at http://localhost:${port}`)
+});
